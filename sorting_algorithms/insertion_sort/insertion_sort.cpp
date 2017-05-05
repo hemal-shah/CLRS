d117f2f83889a09a4848d9535a429153c7c51b56
Hemal Shah <hemal.shah1996@gmail.com>
2017-05-05 12:04:05 +0530

    Changes -_-

---
+++ sorting_algorithms/insertion_sort/insertion_sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+int main(){
+	int n;
+	std::cout<<"Enter number of elements to sort: " ;
+	std::cin>>n;
+	int arr[n];
+	for(int i = 0 ; i < n; i++){
+		//taking inputs..
+		std::cout<<"Enter element number "<<(i+1)<<" : ";
+		std::cin>>arr[i];
+	}
+
+	//implementation of insertion sort starts here...
+	for(int j = 1; j < n; j++){
+		int key = arr[j];
+		int i = j-1;
+		while(i >= 0 && arr[i] > key){
+			arr[i+1] = arr[i];
+			i--;
+		}
+		arr[i+1] = key;
+	}
+
+	//printing the sorted sequence.
+	for(int i = 0; i < n; i++)
+		std::cout<<arr[i]<<" ";
+
+	std::cout<<std::endl;
+	return 0;
+}
