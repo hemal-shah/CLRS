049882706a8a9bc857dadc5d38cc8953a8f16888
Hemal Shah <hemal.shah1996@gmail.com>
2017-05-09 13:14:04 +0530

    Added maximum subarray program

---
+++ divide_and_conquer/max_subarray.py
@@ -0,0 +1,66 @@
+A = list()
+
+#Reading the data first!
+fileObject = open("input.txt", "r+")
+
+data = fileObject.read()
+fileObject.close()
+
+A = data.split("\n")
+A = map(int, A[:len(A) - 1])
+
+
+def print_subarray(low, high):
+    print A[low : high + 1]
+
+def find_max_crossing_subarray(low, mid, high):
+
+    # print "Got the subarray :"
+    # print_subarray(low, high)
+
+    left_sum = -200000
+    sum = 0
+    max_left = -1
+    for i in xrange(mid, low, -1):
+        sum = sum + A[i]
+        if sum > left_sum:
+            left_sum = sum
+            max_left = i
+
+    right_sum = -200000
+    sum = 0
+    max_right = -1
+    for j in range(mid + 1, high + 1):
+        sum = sum + A[j]
+        if sum > right_sum:
+            right_sum = sum
+            max_right = j
+
+    return max_left, max_right, (left_sum + right_sum)
+
+def find_max_subarray(low, high):
+    if (low == high):
+        return low, high, A[low]
+
+    else :
+        mid = int((low + high) / 2)
+        # print "%d\t%d\t%d"%(low, mid, high)
+        left_low, left_high, left_sum = find_max_subarray(low, mid)
+        right_low, right_high, right_sum = find_max_subarray(mid + 1, high)
+        cross_low, cross_high, cross_sum = find_max_crossing_subarray(low, mid, high)
+
+        if left_sum >= right_sum and left_sum >= cross_sum:
+            return left_low, left_high, left_sum
+        elif right_sum >= left_sum and right_sum >= cross_sum:
+            return right_low, right_high, right_sum
+        else :
+            return cross_low, cross_high, cross_sum
+
+# print "Low\tMid\tHigh"
+
+low, high, sum = find_max_subarray(0, len(A) - 1)
+
+print "What we got is :"
+print "Low Index = ", low
+print "High Index = ", high
+print "Max sum = ", sum
--- sorting_algorithms/merge_sort/merge_sort.cpp
+++ sorting_algorithms/merge_sort/merge_sort.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<fstream>
 
 std::vector<int> A;
 void write_vector(std::vector<int> v);
@@ -8,81 +9,85 @@ void merge(int start, int middle, int end);
 
 int main(int argc, char const *argv[]) {
 
-  int n;
-  std::cout << "How many elements you want to insert?" << '\n';
-  std::cin >> n;
+    int n = 0;
+    int data;
+    std::fstream input;
+    input.open("../input.txt");
 
-  for (int i = 0; i < n; i++) {
-    int temp;
-    std::cin >> temp;
-    A.push_back(temp);
-  }
+    input>>data;
+    while(input){
+        A.push_back(data);
+        n++;
+        input>>data;
+    }
 
-  std::cout << "Your input is : " << '\n';
-  write_vector(A);
+    input.close();
 
-  merge_sort(0, n - 1);
+    std::cout << "Your input is : " << '\n';
+    write_vector(A);
 
-  std::cout << "The sorted vector is : " << '\n';
-  write_vector(A);
-  return 0;
+    merge_sort(0, n - 1);
+
+    std::cout << "The sorted vector is : " << '\n';
+    write_vector(A);
+    return 0;
 }
 
 
 void write_vector(std::vector<int> v) {
-  std::vector<int>::iterator itr = v.begin();
-  while(itr != v.end()){
-    std::cout << *itr << ' ';
-    itr++;
-  }
-  std::cout << '\n';
+    std::vector<int>::iterator itr = v.begin();
+    while(itr != v.end()){
+        std::cout << *itr << ' ';
+        itr++;
+    }
+    std::cout << '\n';
 }
 
 
 void merge_sort(int start, int end){
-  if (start < end){
-    int middle = (start + end) / 2;
-    merge_sort(start, middle);
-    merge_sort(middle + 1, end);
-    merge(start, middle, end);
-  }
+    if (start < end){
+        int middle = (start + end) / 2;
+        merge_sort(start, middle);
+        merge_sort(middle + 1, end);
+        merge(start, middle, end);
+    }
 }
 
 
 void merge(int start, int middle, int end){
-  int num1 = middle - start + 1;
-  int num2 = end - middle;
-  std::vector<int> left, right;
+    int num1 = middle - start + 1;
+    int num2 = end - middle;
+    std::vector<int> left, right;
 
-  for (int i = 0; i < num1; i++) {
-    left.push_back(A[i + start]);
-  }
-
-  for(int j = 0; j < num2; j++){
-    right.push_back(A[middle + j + 1]);
-  }
-
-  int i = 0, j = 0, k = start;
-  while (i < num1 && j < num2) {
-    if(left[i] <= right[j]){
-      A[k] = left[i];
-      i++;
-    } else{
-      A[k] = right[j];
-      j++;
+    for (int i = 0; i < num1; i++) {
+        left.push_back(A[i + start]);
     }
-    k++;
-  }
 
-  while(i < num1){
-    A[k] = left[i];
-    i++;
-    k++;
-  }
+    for(int j = 0; j < num2; j++){
+        right.push_back(A[middle + j + 1]);
+    }
 
-  while(j < num2){
-    A[k] = right[j];
-    j++;
-    k++;
-  }
+    int i = 0, j = 0, k = start;
+    while (i < num1 && j < num2) {
+        if(left[i] <= right[j]){
+            A[k] = left[i];
+            i++;
+        } else{
+            A[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < num1){
+        A[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while(j < num2){
+        A[k] = right[j];
+        j++;
+        k++;
+    }
 }
